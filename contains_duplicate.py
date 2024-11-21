7346907463d7c25460347bf4fe60d9daa55be8b3
Sadikur Rahman <sadikur.rahman@bevycommerce.com>
2024-11-21 12:29:21 +0600

    Update contains_duplicate.py

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -1,5 +1,17 @@
+# Solution 1:
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
          numsLength = len(nums)
          numsToSet = set(nums)
          return False if len(list(numsToSet)) == numsLength else True
+
+
+# Solution 2:
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hashSet = set()
+        for num in nums:
+            if num in hashSet:
+                return True
+            hashSet.add(num)
+        return False
