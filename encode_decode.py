7980dfe5725bbdbb62d8b4ff8909bd29fbec4757
Sadikur Rahman <sadikur.rahman@bevycommerce.com>
2024-11-22 15:58:26 +0600

    Update encode_decode.py

--- encode_decode.py
+++ encode_decode.py
@@ -41,3 +41,41 @@ class Solution:
             res.append(s[i:i+sz])
             i += sz
         return res
+
+# Time Complexity of Solution 1 - O(n)
+# Space Complexity of Solution 1 - O(m)
+
+# Solution 2:
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+
+        current = ""
+        for word in strs:
+            current += str(len(word)) + "#" + word
+
+        return current;
+
+    def decode(self, s: str) -> List[str]:
+        if not s:
+            return []
+
+        res, i = [], 0
+
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i:j])
+            i = j + 1
+            j = i + length
+            res.append(s[i:j])
+            i = j
+
+        return res
+
+# Time Complexity of Solution 2 - O(n)
+# Space Complexity of Solution 2 - O(1)
