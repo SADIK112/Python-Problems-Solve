a1c2805aaf47c59a5d62e54529168cc9addbbbef
Sadikur Rahman <sadikur.rahman@bevycommerce.com>
2024-11-24 00:55:25 +0600

    Update valid_palindrom.py

--- valid_palindrom.py
+++ valid_palindrom.py
@@ -36,3 +36,29 @@ class Solution:
         
 # Time Complexity of Solution 1 - O(n)
 # Space Complexity of Solution 1 - O(1)
+
+# Solution 3:
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not self.isAlphaNum(s[l]):
+                l += 1
+
+            while r > l and not self.isAlphaNum(s[r]):
+                r -= 1
+            
+            if s[l].lower() != s[r].lower():
+                return False
+
+            l += 1
+            r -= 1
+        return True
+
+    def isAlphaNum(self, c):
+        return (ord("A") <= ord(c) <= ord("Z") or ord("a") <= ord(c) <= ord("z") or ord("0") <= ord(c) <= ord("9"))
+
+# Time Complexity of Solution 1 - O(n)
+# Space Complexity of Solution 1 - O(1)
