952efde39253485f2620ef5143d7d4196c22d234
Sadikur Rahman <sadikur.rahman@bevycommerce.com>
2024-11-30 17:43:32 +0600

    Create find-target-in-rotated-sorted-array.py

---
+++ find-target-in-rotated-sorted-array.py
@@ -0,0 +1,27 @@
+# Solution
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if target == nums[mid]:
+                return mid
+
+            # check left sorted options
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return -1
+
+# Time Complexity of Solution - O(log n)
+# Space Complexity of Solution - O(1)
