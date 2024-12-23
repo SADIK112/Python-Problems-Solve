7061099bc194bc329af8d96976fb38feeab23f7e
Sadikur Rahman <sadikur.rahman@bevycommerce.com>
2024-12-23 16:11:16 +0600

    Create lowest-common-ancestor-in-binary-search-tree.py

---
+++ lowest-common-ancestor-in-binary-search-tree.py
@@ -0,0 +1,16 @@
+# Solution
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        current = root
+
+        while current:
+            if p.val > current.val and q.val > current.val:
+                current = current.right
+            elif p.val < current.val and q.val < current.val:
+                current = current.left
+            else:
+                return current
+
+# Time Complexity of Solution 1 - O(n)
+# Space Complexity of Solution 1 - O(1)
