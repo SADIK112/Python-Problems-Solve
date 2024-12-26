bc647005e8b8f29911219eb5f774cda71b33dd5a
Sadikur Rahman <sadikur.rahman@bevycommerce.com>
2024-12-26 22:33:39 +0600

    Update count-good-nodes-in-binary-tree.py

--- count-good-nodes-in-binary-tree.py
+++ count-good-nodes-in-binary-tree.py
@@ -1,4 +1,4 @@
-# Solution
+# Solution - DFS
 
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
@@ -14,6 +14,32 @@ class Solution:
             return res
         
         return dfs(root, root.val)
+
+# Time Complexity of Solution- O(n)
+# Space Complexity of Solution - O(n)
+
+# Solution - BFS
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        count = 0
+        q = collections.deque()
+
+        q.append((root, -float('inf')))
+
+        while q:
+            node, maxVal = q.popleft()
+            
+            if node.val >= maxVal:
+                count += 1
+
+            if node.left:
+                q.append((node.left, max(maxVal, node.val)))
+
+            if node.right:
+                q.append((node.right, max(maxVal, node.val)))
+
+        return count
 
 # Time Complexity of Solution- O(n)
 # Space Complexity of Solution - O(n)
